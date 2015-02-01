4a6e832ed1e8718aca42f85a0f2d43973835f44f
deverseli800 <cesar.devers@gmail.com>
2015-02-01 12:04:33 -0500

    Initial Commit

---
+++ Flourish-Teaching/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Flourish-Teaching
+//
+//  Created by Cesar Devers on 2/1/15.
+//  Copyright (c) 2015 Cesar Devers. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
